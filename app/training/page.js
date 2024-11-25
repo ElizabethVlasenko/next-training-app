319fd7d6691fa75a0b54d9f49452b4ddc842fe45
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2024-11-25 17:37:39 +0000

    Add a login mode to the form through searchParams

--- app/training/page.js
+++ app/training/page.js
@@ -5,7 +5,7 @@ import { redirect } from "next/navigation";
 export default async function TrainingPage() {
   const result = await verifyAuth();
 
-  if (result.user) {
+  if (!result.user) {
     return redirect("/");
   }
 
